469c179dc7c167d9998e0639f5af612c7da6edbe
MoretS <s.moret@ecole-ipssi.net>
2021-10-11 12:33:48 +0200

    Add new asset to assets pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join("node_modules")
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w(decidim/admin/initiatives-extras.css)
+Rails.application.config.assets.precompile += %w(decidim/admin/initiatives-extras.css decidim/create_initiative/attachments_error.js)
