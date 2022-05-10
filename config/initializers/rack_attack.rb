e4ab80bebbb0ab46f2ff10e18e2ca50f65d96208
Armand Fardeau <fardeauarmand@gmail.com>
2022-05-10 08:47:11 +0200

    Add logger for rack attack

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -2,3 +2,12 @@
 
 Rack::Attack.safelist_ip(ENV["POD_IP"]) if ENV["POD_IP"].present?
 Rack::Attack.enabled = false if ENV.fetch("RACK_ATTACK_DISABLED", false)
+
+if Rails.env.production?
+  class Rack::Attack
+    throttle("req/ip", limit: Decidim.throttling_max_requests, period: Decidim.throttling_max_requests) do |req|
+      Rails.logger.warn("[Rack::Attack] [THROTTLE - req / ip] :: #{req.ip} :: #{req.path} :: #{req.GET}")
+      req.ip unless req.path.start_with?("/assets")
+    end
+  end
+end
