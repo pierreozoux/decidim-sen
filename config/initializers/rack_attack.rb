a61c39e083e3ef799299d7174025e3dc9486f1a5
Armand Fardeau <fardeauarmand@gmail.com>
2022-05-10 09:40:07 +0200

    Fix rack attack condition

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rack::Attack.safelist_ip(ENV["POD_IP"]) if ENV["POD_IP"].present?
-Rack::Attack.enabled = false if ENV.fetch("RACK_ATTACK_DISABLED", false)
+Rack::Attack.enabled = false if ENV.fetch("RACK_ATTACK_DISABLED", true)
 
 if Rails.env.production?
   class Rack::Attack
