60e2c937a027301a718d35ab224b889649484dc1
Pierre Ozoux <pierre@ozoux.net>
2022-06-02 17:50:33 +0200

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :info
+  config.log_level = :debug
   config.lograge.ignore_actions = ["HealthCheck::HealthCheckController#index"]
 
   # Prepend all log lines with the following tags.
